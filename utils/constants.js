9993e5d688db3725aa0e49f4b826f606a3ecbde3
Vadim <boss.abyshev@gmail.com>
2023-10-16 22:40:10 +0500

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -6,6 +6,6 @@ const limiter = rateLimit({
 });
 
 const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
-const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const emailRegex = /^\S+@\S+\.\S+$/;
 
 module.exports = { urlRegex, emailRegex, limiter };
